cea33e8329e63c6adb4fbefec1f28383a5c21b4c
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2024-02-20 14:54:01 -0500

    ToDo routes started

--- routes/toDo.mjs
+++ routes/toDo.mjs
@@ -4,4 +4,26 @@ import ToDo from '../models/toDo.mjs'
 const router = express.Router()
 
 // Find all ToDo List Items
-router.get
+router.get("/", async(req, res) =>{
+    let foundItems = await ToDo.find()
+    res.status(200).json({
+        data: foundItems
+    })
+})
+
+// Delete a record by ID
+router.delte("/:id", async(req, res) =>{
+    await ToDo.findByIdAndDelete(req.params.id)
+    res.status(204).json({
+        data: "Item was deleted"
+    })
+})
+
+// Create a new ToDo List Item
+router.post("/", async(req, res) =>{
+    const newToDo = new ToDo(req.body)
+    await newToDo.save()
+    res.status(201).json({})
+})
+
+export default router
