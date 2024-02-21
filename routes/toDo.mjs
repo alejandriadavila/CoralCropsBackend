41338e5a0f3c9a33a348eff528b8dfbb17a10437
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2024-02-21 13:00:34 -0500

    CRUD routes for toDo list

--- routes/toDo.mjs
+++ routes/toDo.mjs
@@ -4,26 +4,50 @@ import ToDo from '../models/toDo.mjs'
 const router = express.Router()
 
 // Find all ToDo List Items
-router.get("/", async(req, res) =>{
-    let foundItems = await ToDo.find()
-    res.status(200).json({
-        data: foundItems
+router
+    .route("/")
+    .get(async(req, res) =>{
+        let foundItems = await ToDo.find()
+        res.status(200).json({
+            data: foundItems
+        })
     })
-})
-
-// Delete a record by ID
-router.delte("/:id", async(req, res) =>{
-    await ToDo.findByIdAndDelete(req.params.id)
-    res.status(204).json({
-        data: "Item was deleted"
+    .post(async(req, res, next) => {
+        if (req.body.id && req.body.name && req.body.type && req.body.seedName && req.body.seedCost && req.body.growthTime && req.body.regrowBoolean && req.body.regrowTime && req.body.imgUrl){
+            const crop = {
+                id: req.body.id,
+                name: req.body.name,
+                type: req.body.type,
+                seedName: req.body.seedName,
+                seedCost: req.body.seedCost,
+                growthTime: req.body.growthTime,
+                regrowBoolean: req.body.regrowBoolean,
+                regrowTime: req.body.regrowTime,
+                imgUrl: req.body.imgUrl
+            }
+            await ToDo.push(crop)
+            res.send("Crop added to Crop Planner")
+        }else next(error(400, "Insufficient Data"))
     })
-})
 
-// Create a new ToDo List Item
-router.post("/", async(req, res) =>{
-    const newToDo = new ToDo(req.body)
-    await newToDo.save()
-    res.status(201).json({})
-})
+router
+    .route("/:id")
+    .get(async(req, res) =>{
+        let foundItems = await ToDo.findById(req.params.id)
+        res.status(200).json({
+            data: foundItems
+        })
+    })
+    .patch(async(req, res) => {
+        if (req.body.id && req.body.name && req.body.type && req.body.seedName && req.body.seedCost && req.body.growthTime && req.body.regrowBoolean && req.body.regrowTime && req.body.imgUrl){
+            await ToDo.findOneAndUpdate(req.params.id, req.body)
+        }
+    })
+    .delete(async(req, res) =>{
+        await ToDo.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            data: "Item was deleted"
+        })
+    })
 
 export default router
